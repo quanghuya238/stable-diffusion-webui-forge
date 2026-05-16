a1682bcde1c62f6f7b6615d52b880db4e2fb168d
Huy <quanghuya238@gmail.com>
2026-05-16 21:15:48 +0700

    fix: filter invalid ControlNet units instead of asserting

--- extensions-builtin/sd_forge_controlnet/scripts/controlnet.py
+++ extensions-builtin/sd_forge_controlnet/scripts/controlnet.py
@@ -104,7 +104,7 @@ class ControlNetForForgeOfficial(scripts.Script):
             ControlNetUnit.from_dict(unit) if isinstance(unit, dict) else unit
             for unit in units
         ]
-        assert all(isinstance(unit, ControlNetUnit) for unit in units)
+        units = [unit for unit in units if isinstance(unit, ControlNetUnit)]
         enabled_units = [x for x in units if x.enabled]
         return enabled_units
 
